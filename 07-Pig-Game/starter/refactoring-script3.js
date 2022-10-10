4dab603091994496c440e5563f17c49a48d699bd
이동건 <developer@idong-geon-ui-MacBookPro.local>
2022-10-10 13:12:39 +0900

    pig app 리팩토링

---
+++ 07-Pig-Game/starter/refactoring-script3.js
@@ -0,0 +1,151 @@
+const _doc = message => document.querySelector(message);
+
+const score0El = _doc('#score--0');
+const score1El = _doc('#score--1');
+const diceEl = _doc('.dice');
+const btnRoll = _doc('.btn--roll');
+const btnHold = _doc('.btn--hold');
+const btnNewGame = _doc('.btn--new');
+const current0El = _doc('#current--0');
+const current1El = _doc('#current--1');
+const player0 = _doc('.player--0');
+const player1 = _doc('.player--1');
+
+scores = [0, 0];
+let score0 = 0;
+let score1 = 0;
+
+let cur0Score = 0;
+let cur1Score = 0;
+
+let activePlayer = 0;
+let playing = true;
+
+score0El.textContent = 0;
+score1El.textContent = 0;
+diceEl.style.display = `none`;
+
+const sumScorePlayer0 = randomNumber => {
+  score0 += randomNumber;
+  cur0Score = score0;
+  current0El.textContent = cur0Score - scores[activePlayer];
+};
+
+const sumScorePlayer1 = randomNumber => {
+  score1 += randomNumber;
+  cur1Score = score1;
+  current1El.textContent = cur1Score - scores[activePlayer];
+};
+
+const switchPlayer0 = total => {
+  score0 = total;
+  current0El.textContent = score0 - total;
+  player0TurnOff();
+};
+
+const switchPlayer1 = total => {
+  score1 = total;
+  current1El.textContent = score1 - total;
+  player1TurnOff();
+};
+
+const player0TurnOff = () => {
+  activePlayer = 1;
+  player0.classList.remove('player--active');
+  player1.classList.add('player--active');
+};
+
+const player1TurnOff = () => {
+  activePlayer = 0;
+  player1.classList.remove('player--active');
+  player0.classList.add('player--active');
+};
+
+const winnerPlayer0Game = () => {
+  playing = false;
+  diceEl.style.display = `none`;
+  player0.classList.add('player--winner');
+};
+
+const winnerPlayer1Game = () => {
+  playing = false;
+  diceEl.style.display = `none`;
+  player1.classList.add('player--winner');
+};
+
+const displayPlayer0Score = score0 => {
+  current0El.textContent = cur0Score = 0;
+  score0El.textContent = scores[activePlayer] = score0;
+  player0TurnOff();
+};
+
+const displayPlayer1Score = score1 => {
+  current1El.textContent = cur1Score = 0;
+  score1El.textContent = scores[activePlayer] = score1;
+  player1TurnOff();
+};
+
+const currentPlayer = (element, className) => {
+  return element.classList.contains(className);
+};
+
+// ! JS값이 변경되면 UI에 해당 값을 element의 textContent에 할당해라!
+const resetGame = () => {
+  activePlayer = scores[0] = scores[1] = score0 = score1 = 0;
+
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+  current0El.textContent = 0;
+  current1El.textContent = 0;
+
+  playing = true;
+  player0.classList.add('player--active');
+  player1.classList.remove('player--active');
+  player0.classList.remove('player--winner');
+  player1.classList.remove('player--winner');
+  diceEl.style.display = `none`;
+};
+
+btnNewGame.addEventListener('click', resetGame);
+
+btnRoll.addEventListener('click', () => {
+  if (playing) {
+    let randomNumber = Math.trunc(Math.random() * 6 + 1);
+    diceEl.style.display = `block`;
+    diceEl.src = `dice-${randomNumber}.png`;
+
+    // 플레이어 0
+    if (currentPlayer(player0, 'player--active')) {
+      randomNumber === 1
+        ? switchPlayer0(scores[activePlayer])
+        : sumScorePlayer0(randomNumber);
+    }
+
+    // 플레이어 1
+    else if (currentPlayer(player1, 'player--active')) {
+      randomNumber === 1
+        ? switchPlayer1(scores[activePlayer])
+        : sumScorePlayer1(randomNumber);
+    }
+  } else {
+    console.log('게임이 종료되었습니다. 재시작 버튼을 눌러주세요!');
+  }
+});
+
+btnHold.addEventListener('click', () => {
+  if (playing) {
+    // 플레이어 0
+    if (currentPlayer(player0, 'player--active')) {
+      score0 > 30 && winnerPlayer0Game();
+      displayPlayer0Score(score0);
+    }
+
+    // 플레이어 1
+    else if (currentPlayer(player1, 'player--active')) {
+      score1 > 30 && winnerPlayer1Game();
+      displayPlayer1Score(score1);
+    }
+  } else {
+    console.log('게임이 종료되었습니다. 재시작 버튼을 눌러주세요!');
+  }
+});
--- 07-Pig-Game/starter/script2.js
+++ 07-Pig-Game/starter/script2.js
@@ -21,12 +21,13 @@ let score1 = 0;
 // 진행 중인 현재 플레이어
 let activePlayer = 0;
 
-// 0번 현재점수
+// 0번 플레이어 현재점수
 let cur0Score = 0;
 
-// 1번 현재점수
+// 1번 플레이어 현재점수
 let cur1Score = 0;
 
+// 게임 진행 중
 let playing = true;
 
 // 더미 데이터 초기화
@@ -34,6 +35,10 @@ score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.style.display = `none`;
 
+// ! 요약 : 주사위 1이 나오면 기존에 누적된 값을 score0에 저장해야된다.
+// ! score0는 항상 모든 값이 다 저장되어 있으므로 current0El에서는
+// ! 0번 플레이어에게 저장되어 있는 값을 뺴야 0번 current 패널에서 주사위를
+// ! 굴릴 떄, 0부터 값이 올라가기 시작합니다.
 btnRoll.addEventListener('click', () => {
   let randomNumber = Math.trunc(Math.random() * 6 + 1);
 
@@ -43,8 +48,8 @@ btnRoll.addEventListener('click', () => {
 
     if (player0.classList.contains('player--active')) {
       if (randomNumber === 1) {
-        cur0Score = score0 = scores[activePlayer];
-        current0El.textContent = cur0Score - scores[activePlayer];
+        score0 = scores[activePlayer];
+        current0El.textContent = score0 - scores[activePlayer];
 
         activePlayer = 1;
         player0.classList.remove('player--active');
@@ -54,10 +59,13 @@ btnRoll.addEventListener('click', () => {
         cur0Score = score0;
         current0El.textContent = cur0Score - scores[activePlayer];
       }
-    } else if (player1.classList.contains('player--active')) {
+    }
+
+    // 이하 같음
+    else if (player1.classList.contains('player--active')) {
       if (randomNumber === 1) {
-        cur1Score = score1 = scores[activePlayer];
-        current1El.textContent = cur1Score - scores[activePlayer];
+        score1 = scores[activePlayer];
+        current1El.textContent = score1 - scores[activePlayer];
 
         activePlayer = 0;
         player1.classList.remove('player--active');
@@ -74,18 +82,22 @@ btnRoll.addEventListener('click', () => {
 btnHold.addEventListener('click', () => {
   if (playing) {
     if (player0.classList.contains('player--active')) {
-      if (score0 > 20) {
+      if (score0 > 30) {
         playing = false;
         diceEl.style.display = `none`;
         player0.classList.add('player--winner');
       }
+
       current0El.textContent = cur0Score = 0;
       score0El.textContent = scores[activePlayer] = score0;
       activePlayer = 1;
       player0.classList.remove('player--active');
       player1.classList.add('player--active');
-    } else if (player1.classList.contains('player--active')) {
-      if (score1 > 20) {
+    }
+
+    // 이하 같음
+    else if (player1.classList.contains('player--active')) {
+      if (score1 > 30) {
         playing = false;
         diceEl.style.display = `none`;
         player1.classList.add('player--winner');
@@ -100,13 +112,14 @@ btnHold.addEventListener('click', () => {
   }
 });
 
+// ! JS값이 변경되면 UI에 해당 값을 element의 textContent에 할당해라!
 btnNewGame.addEventListener('click', () => {
-  cur0Score = cur1Score = score0 = score1 = 0;
-  activePlayer = scores[0] = scores[1] = 0;
-  current0El.textContent = cur0Score;
-  current1El.textContent = cur1Score;
+  activePlayer = scores[0] = scores[1] = score0 = score1 = 0;
+
   score0El.textContent = score0;
   score1El.textContent = score1;
+  current0El.textContent = score0;
+  current1El.textContent = score1;
 
   playing = true;
   player0.classList.add('player--active');
