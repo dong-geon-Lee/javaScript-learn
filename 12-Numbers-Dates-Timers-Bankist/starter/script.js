d8bdb1ad96c52f34185acad6fe855a73e7c65c1f
이동건 <developer@idong-geon-ui-MacBookPro.local>
2022-10-17 20:10:15 +0900

    dom & event learning

--- 12-Numbers-Dates-Timers-Bankist/starter/script.js
+++ 12-Numbers-Dates-Timers-Bankist/starter/script.js
@@ -16,17 +16,18 @@ const account1 = {
   pin: 1111,
 
   movementsDates: [
-    '2019-11-18T21:31:17.178Z',
-    '2019-12-23T07:42:02.383Z',
+    '2022-10-16T21:31:17.178Z',
+    '2022-10-15T07:42:02.383Z',
     '2020-01-28T09:15:04.904Z',
-    '2020-04-01T10:17:24.185Z',
+    '2022-10-13T10:17:24.185Z',
     '2020-05-08T14:11:59.604Z',
-    '2020-05-27T17:01:17.194Z',
-    '2020-07-11T23:36:17.929Z',
-    '2020-07-12T10:51:36.790Z',
+    '2022-10-14T17:01:17.194Z',
+    '2022-07-15T23:36:17.929Z',
+    '2022-10-16T10:51:36.790Z',
   ],
   currency: 'EUR',
   locale: 'pt-PT', // de-DE
+  // locale: 'KO', // de-DE
 };
 
 const account2 = {
@@ -80,21 +81,55 @@ const inputClosePin = document.querySelector('.form__input--pin');
 
 /////////////////////////////////////////////////
 // Functions
+const formatMovementDate = (date, locale) => {
+  const calcDaysPassed = (date1, date2) => {
+    return Math.round(Math.abs(date2 - date1) / (1000 * 60 * 60 * 24));
+  };
 
-const displayMovements = function (movements, sort = false) {
+  const daysPassed = calcDaysPassed(new Date(), date);
+  console.log(daysPassed);
+
+  if (daysPassed === 0) return 'Today';
+  if (daysPassed === 1) return 'Yesterday';
+  if (daysPassed <= 7) return `${daysPassed} days ago`;
+
+  // const day = `${date.getDate()}`.padStart(2, 0);
+  // const month = `${date.getMonth() + 1}`.padStart(2, 0);
+  // const year = date.getFullYear();
+
+  // return `${year}/${month}/${day}`;
+  return new Intl.DateTimeFormat(locale).format(date);
+};
+
+const formatCur = (value, locale, currency) => {
+  return new Intl.NumberFormat(locale, {
+    style: 'currency',
+    currency: currency,
+  }).format(value);
+};
+
+const displayMovements = function (acc, sort = false) {
   containerMovements.innerHTML = '';
 
-  const movs = sort ? movements.slice().sort((a, b) => a - b) : movements;
+  const movs = sort
+    ? acc.movements.slice().sort((a, b) => a - b)
+    : acc.movements;
 
   movs.forEach(function (mov, i) {
     const type = mov > 0 ? 'deposit' : 'withdrawal';
 
+    const date = new Date(acc.movementsDates[i]);
+    const displayDate = formatMovementDate(date, acc.locale);
+    const formattedMov = formatCur(mov, acc.locale, acc.currency);
+
+    console.log(formattedMov);
     const html = `
       <div class="movements__row">
         <div class="movements__type movements__type--${type}">${
       i + 1
     } ${type}</div>
-        <div class="movements__value">${mov}€</div>
+      <div class="movements__date">${displayDate}</div>
+        <div class="movements__value">${formattedMov}</div>
       </div>
     `;
 
@@ -104,20 +139,20 @@ const displayMovements = function (movements, sort = false) {
 
 const calcDisplayBalance = function (acc) {
   acc.balance = acc.movements.reduce((acc, mov) => acc + mov, 0);
-  labelBalance.textContent = `${acc.balance}€`;
+
+  labelBalance.textContent = formatCur(acc.balance, acc.locale, acc.currency);
 };
 
 const calcDisplaySummary = function (acc) {
   const incomes = acc.movements
     .filter(mov => mov > 0)
     .reduce((acc, mov) => acc + mov, 0);
-  labelSumIn.textContent = `${incomes}€`;
+  labelSumIn.textContent = formatCur(incomes, acc.locale, acc.currency);
 
   const out = acc.movements
     .filter(mov => mov < 0)
     .reduce((acc, mov) => acc + mov, 0);
-  labelSumOut.textContent = `${Math.abs(out)}€`;
-
+  labelSumOut.textContent = formatCur(Math.abs(out), acc.locale, acc.currency);
   const interest = acc.movements
     .filter(mov => mov > 0)
     .map(deposit => (deposit * acc.interestRate) / 100)
@@ -126,7 +161,7 @@ const calcDisplaySummary = function (acc) {
       return int >= 1;
     })
     .reduce((acc, int) => acc + int, 0);
-  labelSumInterest.textContent = `${interest}€`;
+  labelSumInterest.textContent = formatCur(interest, acc.locale, acc.currency);
 };
 
 const createUsernames = function (accs) {
@@ -142,7 +177,7 @@ createUsernames(accounts);
 
 const updateUI = function (acc) {
   // Display movements
-  displayMovements(acc.movements);
+  displayMovements(acc);
 
   // Display balance
   calcDisplayBalance(acc);
@@ -151,9 +186,47 @@ const updateUI = function (acc) {
   calcDisplaySummary(acc);
 };
 
+const startLogOutTimer = () => {
+  const tick = () => {
+    const min = String(Math.trunc(time / 60)).padStart(2, 0);
+    const sec = String(time % 60).padStart(2, 0);
+
+    // In each call, print the remaining time to UI
+    labelTimer.textContent = `${min}:${sec}`;
+
+    // When 0 seconds, stop timer and log out user
+    if (time === 0) {
+      console.log(timer, '5초 맞니?');
+      clearInterval(timer);
+      labelWelcome.textContent = `Log in to get started`;
+      containerApp.style.opacity = 0;
+      console.log(timer);
+    }
+
+    // Decrese 1s
+    time--;
+  };
+  // console.log(timer, '여기는?');
+
+  // Set time to 5 minutes
+  let time = 5;
+
+  tick();
+  // Call the timer every second
+  const timer = setInterval(tick, 1000);
+  return timer;
+
+  // return setInterval(tick, 1000);
+};
+
 ///////////////////////////////////////
 // Event handlers
-let currentAccount;
+let currentAccount, timer;
+console.log(timer, '타이머');
+// fake always logged in
+// currentAccount = account1;
+// updateUI(currentAccount);
+// containerApp.style.opacity = 100;
 
 btnLogin.addEventListener('click', function (e) {
   // Prevent form from submitting
@@ -164,16 +237,52 @@ btnLogin.addEventListener('click', function (e) {
   );
   console.log(currentAccount);
 
-  if (currentAccount?.pin === Number(inputLoginPin.value)) {
+  if (currentAccount?.pin === +inputLoginPin.value) {
     // Display UI and message
     labelWelcome.textContent = `Welcome back, ${
       currentAccount.owner.split(' ')[0]
     }`;
     containerApp.style.opacity = 100;
 
+    // Create current date and time
+    // API test
+    // ? 유용한 날짜변환 ! 이런건 생각도 못했다.
+    const nows = new Date();
+    const options = {
+      year: 'numeric',
+      month: 'long',
+      day: 'numeric',
+      hour: 'numeric',
+      minute: 'numeric',
+      weekday: 'long',
+    };
+
+    labelDate.textContent = new Intl.DateTimeFormat(
+      currentAccount.locale,
+      options
+    ).format(nows);
+
+    // 오전 시간:분 을 표시한다
+    // labelDate.textContent = new Intl.DateTimeFormat('KO', options).format(nows);
+    console.log(new Intl.DateTimeFormat('KO').format(nows));
+
+    // 리팩토링 이전 날짜 수식
+    // console.log(new Intl.DateTimeFormat('KO').format(nows));
+
+    // const year = nows.getFullYear();
+    // const month = `${nows.getMonth() + 1}`.padStart(2, 0);
+    // const day = `${nows.getDate()}`.padStart(2, 0);
+    // const hour = `${nows.getHours()}`.padStart(2, 0);
+    // const min = `${nows.getMinutes()}`.padStart(2, 0);
+
+    // labelDate.textContent = `${year}/${month}/${day}, ${hour}:${min}`;
+
     // Clear input fields
     inputLoginUsername.value = inputLoginPin.value = '';
     inputLoginPin.blur();
+
+    if (timer) clearInterval(timer);
+    timer = startLogOutTimer();
 
     // Update UI
     updateUI(currentAccount);
@@ -182,7 +291,7 @@ btnLogin.addEventListener('click', function (e) {
 
 btnTransfer.addEventListener('click', function (e) {
   e.preventDefault();
-  const amount = Number(inputTransferAmount.value);
+  const amount = +inputTransferAmount.value;
   const receiverAcc = accounts.find(
     acc => acc.username === inputTransferTo.value
   );
@@ -198,22 +307,39 @@ btnTransfer.addEventListener('click', function (e) {
     currentAccount.movements.push(-amount);
     receiverAcc.movements.push(amount);
 
+    // Add transfer date
+    currentAccount.movementsDates.push(new Date().toISOString());
+    receiverAcc.movementsDates.push(new Date().toISOString());
+
     // Update UI
     updateUI(currentAccount);
+
+    // Reset timer
+    clearInterval(timer);
+    timer = startLogOutTimer();
   }
 });
 
 btnLoan.addEventListener('click', function (e) {
   e.preventDefault();
 
-  const amount = Number(inputLoanAmount.value);
+  const amount = Math.floor(inputLoanAmount.value);
 
   if (amount > 0 && currentAccount.movements.some(mov => mov >= amount * 0.1)) {
-    // Add movement
-    currentAccount.movements.push(amount);
+    setTimeout(() => {
+      // Add movement
+      currentAccount.movements.push(amount);
 
-    // Update UI
-    updateUI(currentAccount);
+      // Add loan date
+      currentAccount.movementsDates.push(new Date().toISOString());
+
+      // Update UI
+      updateUI(currentAccount);
+
+      // Reset timer
+      clearInterval(timer);
+      timer = startLogOutTimer();
+    }, 2500);
   }
   inputLoanAmount.value = '';
 });
@@ -223,7 +349,7 @@ btnClose.addEventListener('click', function (e) {
 
   if (
     inputCloseUsername.value === currentAccount.username &&
-    Number(inputClosePin.value) === currentAccount.pin
+    +inputClosePin.value === currentAccount.pin
   ) {
     const index = accounts.findIndex(
       acc => acc.username === currentAccount.username
@@ -244,10 +370,209 @@ btnClose.addEventListener('click', function (e) {
 let sorted = false;
 btnSort.addEventListener('click', function (e) {
   e.preventDefault();
-  displayMovements(currentAccount.movements, !sorted);
+  displayMovements(currentAccount, !sorted);
   sorted = !sorted;
 });
 
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
+
+// ! 170강 - Number
+// 가끔 이런 숫자가 있다.
+console.log(0.1 + 0.2);
+
+// ? Conversion 형변환
+console.log(Number('23'));
+console.log(+'23');
+
+// Parsing
+// ? 제일 앞에 숫자만 있으면 어떤 문자열이든 무시하고 숫자만 출력한다.
+console.log(Number.parseInt('500k'));
+console.log(Number.parseInt('500k', 10));
+console.log(Number.parseInt('2.5rem'));
+console.log(Number.parseFloat('2.5rem'));
+
+// isNaN보다 isFinite를 더 권장함
+// console.log(Number.isNaN(20));
+// console.log(Number.isNaN('20'));
+// console.log(Number.isNaN(+'20X'));
+// console.log(Number.isNaN(23 / 0), '무한도 숫자가 아니다.');
+
+// 무한대를 측정하기 위해 사용하기 좋다.
+console.log(Number.isFinite(20));
+console.log(Number.isFinite('20'));
+console.log(Number.isFinite(+'20X'));
+console.log(Number.isFinite(23 / 0), '무한도 숫자가 아니다.');
+
+// console.log(Number.isInteger(23.0));
+// console.log(Number.isInteger(23));
+
+// ! 171강 - Math
+console.log(Math.sqrt(25));
+console.log(25 ** (1 / 2)); // 제곱근
+console.log(8 ** (1 / 3)); // 세제곱근
+console.log(3 ** 2);
+
+console.log(Math.max(5, 18, 3, 2, 1));
+console.log(Math.max(5, 18, 23, 11, 2));
+// ? 문자열을 넣어도 제일 큰 숫자로 나오네?
+console.log(Math.max(5, 18, '23', 11, 2));
+console.log(Math.max(5, 18, '23px', 11, 2));
+
+// 파이함수
+console.log(Math.PI * Number.parseFloat('10px') ** 2);
+console.log(Math.trunc(Math.random() * 6 + 1));
+
+// ? 최소값에서 최대값 사이에 있는 랜덤 숫자, 이거 좋은데??
+const randomInt = (min, max) =>
+  Math.floor(Math.random() * (max - min) + 1) + min;
+console.log(randomInt(100, 150));
+
+// Math 함수들은 유형을 강제로 숫자로 바꾼다.
+// 단순히 소수점 제거
+console.log(Math.trunc(23.3));
+console.log(Math.trunc('23.3'));
+
+// 조건부 소숫점 반올림 round
+console.log(Math.round(23.51));
+console.log(Math.round('23.29'));
+
+// 무조건 소숫점 반올림
+console.log(Math.ceil(26.1));
+console.log(Math.ceil('23.11'));
+
+// 무조건 소숫점 내림
+console.log(Math.floor(25.9));
+console.log(Math.floor('25.99'));
+
+// Rounding decimals
+//? 문자열을 반환 하는것을 주의한다
+console.log((2.7).toFixed(0));
+console.log((2.7).toFixed(3));
+console.log((2.345).toFixed(2));
+console.log(+(2.345).toFixed(2));
+
+// ! 172강 -reminder operator
+console.log(5 % 2);
+console.log(5 / 2);
+
+console.log(8 % 3);
+console.log(8 / 3); // 8 = 2 * 3 + 2
+
+console.log(6 % 2);
+console.log(6 / 2);
+
+console.log(7 % 2);
+console.log(7 / 2);
+
+const isEven = n => n % 2 === 0;
+console.log(isEven(6));
+
+const isOdd = n => n % 2 === 1;
+console.log(isOdd(isOdd(5)));
+
+// ? 흥미롭다. 이렇게 바꾸는 테크릭이 있다니!!!
+labelBalance.addEventListener('click', () => {
+  const movs = [...document.querySelectorAll('.movements__row')];
+  movs.forEach((row, i) => {
+    if ((i + 1) % 2 === 0) row.style.backgroundColor = 'orangered';
+    if ((i + 1) % 2 === 1) row.style.backgroundColor = 'aqua';
+    if ((i + 1) % 3 === 0) row.style.backgroundColor = 'blue';
+  });
+});
+
+// ! 173강 - 숫자 구분 기호
+// ? underscore를 읽지 못하는 특징을 이용해서 쉼표만들 수 있겠다.
+const diameter = '28_000_000';
+console.log(diameter.split('_').join(','));
+
+const priceCents = 345_99;
+console.log(priceCents);
+
+// ! 174강 - BigInt
+console.log(2 ** 53 - 1);
+console.log(Number.MAX_SAFE_INTEGER);
+console.log(2 ** 53 + 1);
+console.log(BigInt(2 ** 55));
+
+// ! 175강 - date & time
+const now = new Date();
+console.log(now);
+console.log(now.getFullYear());
+console.log(now.getMonth());
+console.log(now.getDate());
+console.log(now.getDay());
+console.log(now.getHours());
+console.log(now.getMinutes());
+console.log(now.getSeconds());
+console.log(now.toISOString());
+console.log(Date.now());
+
+// ! 176강 - bankist 날짜 추가 작업
+
+// ! 177강 - 날짜 연산
+// +연산자로 표현해서 시간을 숫자로 변환한게 신선하다
+const future = new Date(2037, 10, 19, 15, 23);
+console.log(+future);
+
+// 1000을 나누는 이유는 밀리초를 초로 바꾸기 위해서다
+// 초 -> 분
+// 분 -> 시간
+// 시간 -> 일
+const calcDaysPassed2 = (date1, date2) => {
+  return Math.abs(date2 - date1) / (1000 * 60 * 60 * 24);
+};
+
+const days1 = calcDaysPassed2(new Date(2037, 3, 4), new Date(2037, 3, 14));
+console.log(days1);
+
+// ! 178강 - 날짜 국제화
+// bankist app 적용
+
+// ! 179강 - 국제화 API 예시
+// 굉장히 유용한 활용 예시다!
+const num = 38294798;
+const options = {
+  style: 'currency',
+  // style: 'unit',
+  // unit: 'mile-per-hour',
+  // unit: 'celsius',
+  currency: 'USD',
+};
+
+// DateTimeFormat 외에 NumberFormat도 있다.
+console.log('K ', new Intl.NumberFormat().format(num));
+console.log('US ', new Intl.NumberFormat('en-US', options).format(num));
+console.log(
+  new Intl.NumberFormat('ko-KR', { style: 'currency', currency: 'KRW' }).format(
+    num
+  )
+);
+// ! 180강 - 타이머 함수 API
+// setTimeout() - 백그라운드에서 이 콜백 함수를 등록하고 해당 시간이 지나면 호출된다. (비동기)
+// ? setTimeout 함수가 인자를 가질 수 있고 인자를 직접 등록해서 사용 할 수 있구나!
+const ingredients = ['olives', 'spinach'];
+
+const pizzaTimer = setTimeout(
+  (ing1, ing2) => {
+    console.log(`Here is your pizza with ${ing1} and ${ing2} 🦂`);
+  },
+  3000,
+  ...ingredients
+);
+
+console.log('Waiting...');
+
+//  clearTimeout() - setTimeout의 작동을 중지시킨다.
+if (ingredients.includes('spinach')) {
+  clearTimeout(pizzaTimer);
+}
+
+// setInterval() - 특정 시간마다 콜백함수를 실행시킨다.
+// setInterval(() => {
+//   const now = new Date();
+//   console.log(now);
+// }, 3000);
+
+// ! 181강 - 카운트 다운 타이머
--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -1,14 +1,159 @@
 'use strict';
+/**
+  자바스크립트 객체는 Node로 나타낸다. Node는 DOM보다 상위유형에 속한다. 
+  Node는 textContent, childNodes, parentNode, cloneNode가 나뉜다.  
+  DOM은 4가지 타입으로 나뉜다. Element, Text, Comment(주석), Document
+  Elment에는 먼저 익숙한 것부터 나열하자면 다음과 같다. 
+  innerHTML, classList, remove(), insertAdjacentHTML(), querySelector()
+  보통인 것은 parentElement, append(), closest(), setAttribute()
+  생소한 것은 children, matches(), scrollIntoView() 이다. 
+  ! DOM의 모든 단일 유형의 노드에는 EventTarget과 window 객체를 상속받기 떄문에 
+  ? addEventListener(), removeEventListener()를 호출 할 수 있다. 또,
+  window에서 가지고 있는 많이 프로퍼티와 메서드에 접근 할 수 있다. 
+  ! 따라서 DOM API는 기본적으로 상속에 의해서 작동한다고 볼 수 있다. 
+  DOM은 DOM 트리와 상호 작용하는 많은 메서드와 속성을 포함하는 매우 복잡한 API입니다.
 
-///////////////////////////////////////
+  DOM (문서 개체 모델 )은 API (응용 프로그래밍 인터페이스 )입니다. 
+  DOM 트리는 DOM API로 표현되는 HTML 문서의 구조입니다. 
+  언급한 바와 같이 이 API는 트리를 조작하는 데 사용할 수 있는 많은 메서드와
+  속성을 제공하므로 더 나아가 문서를 조작할 수 있습니다.
+ */
+// const DOM = document.querySelector('body');
+/** 새로 고침 시 스크롤이 위로 올라가는 문제점이 있다.
+ * a태그의 href 속성값 "#" 에서 발생하는 페이지 새로고침 문제가
+ * 원인인데, preventDefault() 함수를 사용해서 작동을 금지시킨다.
+ */
+// ! Selecting elements
+// 전체 html을 가리킨다.
+// console.log(document.documentElement);
+
+// html의 body
+// console.log(document.body);
+
+// console.log(document.head);
+// console.log(document.querySelector('.header'));
+
+// const allSections = document.querySelectorAll('.section');
+// console.log(allSections);
+
+// id 요소에 접근 할 떄, 사용된다. (#생략됨)
+// console.log(document.getElementById('section--1'));
+
+// html에서 전체 button 태그 리스트를 반환한다.
+// console.log(document.getElementsByTagName('button'));
+
+// ? 전체 button에 스타일을 변경 할 수 있다.
+// const btnList = [...document.getElementsByTagName('button')];
+// btnList.forEach(btn => {
+//   btn.style.backgroundColor = 'aqua';
+// });
+
+// html 태그에 사용된 모든 해당 클래스를 찾는다.
+// console.log(document.getElementsByClassName('highlight'));
+
+// Creating and inserting elements
+// const header = document.querySelector('.header');
+
+// const message = document.createElement('div');
+// message.classList.add('cookie-message');
+// // message.textContent = 'We use cookied for improved functionality and analytics.';
+// message.innerHTML = `We use cookied for improved functionality and analytics.
+//   <button class="btn btn--close-cookie">Got it!</button>`;
+
+// prepend(): 해당 element의 첫 번째 자식으로 삽입한다.
+// append() : 해당 element의 마지막 자식으로 삽입한다.
+// header.prepend(message);
+// ? DOM 요소들을 항상 한 번에 한 장소에만 존재할 수 있다.
+// 따라서 둘 다 동시에 같은 요소에 메서드를 적용 시킬 수 없다.
+// header.prepend(message);
+// header.append(message);
+
+// 단, cloneNode(true)를 사용하면 복사본을 만들수 있다.
+// header.append(message.cloneNode(true));
+
+// 다른 대안도 존재한다. 바로 before()과 after() 메서드다.
+// 이 메서드는 해당 element의 형제 위치에 요소들 위치시킨다.
+// 즉, header 라는 부모가 아닌 형제라고 보면 된다 .
+// header.before(message);
+// header.after(message);
+
+// 그냥 insertAdjacentHTML() 가장 많이 사용되는 메서드 사용하는게 편하겠다.
+
+// Delete elements
+// document.querySelector('.btn--close-cookie').addEventListener('click', () => {
+//   message.remove();
+//    구식 제거 방법
+//    message.parentElement.removeChild(message);
+// });
+
+// ! 187강 - styles, property, class
+// Styles - 인라인 스타일
+// style이 html태그의 class에 적용되어야 콘솔로 조회가능하다.
+// message.style.backgroundColor = '#37383d';
+// message.style.width = '120%';
+// console.log(message.style.height);
+// console.log(message.style.backgroundColor);
+
+// getComputedStyle을 이용해서 앱의 width와 height 아는건 유용할듯
+// console.log(getComputedStyle(message).color);
+// console.log(getComputedStyle(message).width);
+// console.log(getComputedStyle(message).height);
+
+// 이게 중요한건지? 잘 모르겠네...
+// message.style.height = parseInt(getComputedStyle(message).height) + 'px';
+// console.log(message.style.height);
+
+// 앱에 지정된 특정 클래스 전부를 바꿀 수 있는 매력적인 기능이다.
+// 상황에 따라서 굉장히 편리할 가능성이 높다.
+// document.documentElement.style.setProperty('--color-primary', 'orangered');
+
+// Attributes
+// const logo = document.querySelector('.nav__logo');
+// console.log(logo.src);
+// console.log(logo.alt);
+// console.log(logo.className);
+
+// Non-standard : 해당 element의 표준 속성이 아니면 undefined가 출력된다.
+// 수동으로 값을 추가해주면 조회된다.
+// logo.designer = 'lee';
+// console.log(logo.designer);
+
+// setAttribute도 element의 key와 value를 직접 추가해줄 수 있다.
+// 속성의 값을 가져오는 속성은 getAttribute를 사용한다.
+// logo.setAttribute('company', 'Bankist');
+// console.log(logo.getAttribute('company'));
+// console.log(logo.getAttribute('src'));
+
+// 값을 가져오는 차이점이 있다.
+// const link = document.querySelector('.nav__link--btn');
+// console.log(link);
+
+// 절대 경로 URL 반환
+// console.log(link.href);
+
+// HTML Tag 내부에서 href 속성의 값 반환 -> '#'
+// console.log(link.getAttribute('href'));
+
+// Data attributes
+// HTML에서 data- 로 지정된 예약어 js에서 dataset으로 쓴다 (카멜케이스)
+// console.log(logo.dataset.versionNumber);
+
+// Classes
+// logo.classList.add('c');
+// logo.classList.remove('c');
+// logo.classList.toggle('c');
+// logo.classList.contains('c');
+// logo.classList.closest('c');
 // Modal window
+///////////////////////////////////////
 
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
 
-const openModal = function () {
+const openModal = function (e) {
+  e.preventDefault();
   modal.classList.remove('hidden');
   overlay.classList.remove('hidden');
 };
@@ -18,9 +163,7 @@ const closeModal = function () {
   overlay.classList.add('hidden');
 };
 
-for (let i = 0; i < btnsOpenModal.length; i++)
-  btnsOpenModal[i].addEventListener('click', openModal);
-
+btnsOpenModal.forEach(btn => btn.addEventListener('click', openModal));
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
 
@@ -28,4 +171,99 @@ document.addEventListener('keydown', function (e) {
   if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
     closeModal();
   }
+});
+
+//! 188강 - 스크롤
+const btnScorollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
+
+// getBoundingClientRect(), window.pageXOffset, window.pageYOffset
+// document.documentElement.clientWidth, document.documentElement.clientHeight
+// window.scrollTo()
+btnScorollTo.addEventListener('click', e => {
+  const s1coords = section1.getBoundingClientRect();
+  console.log(s1coords);
+  console.log(e.target.getBoundingClientRect());
+  console.log('Current scroll (X/Y)', window.pageXOffset, window.pageYOffset);
+  console.log(
+    'height/width viewPort',
+    document.documentElement.clientHeight,
+    document.documentElement.clientWidth
+  );
+
+  // old pattern 1
+  // Scrolling
+  // window.scrollTo(
+  //   s1coords.left + window.pageXOffset,
+  //   s1coords.top + window.pageYOffset
+  // );
+
+  // old pattern 2
+  // window.scrollTo({
+  //   left: s1coords.left + window.pageXOffset,
+  //   top: s1coords.top + window.pageYOffset,
+  //   behavior: 'smooth',
+  // });
+
+  section1.scrollIntoView({ behavior: 'smooth' });
+});
+
+//! 189강 - 이벤트
+const h1 = document.querySelector('h1');
+
+const alertH1 = e => {
+  alert('addEventListener: Great! You are reading the heading :D');
+
+  // h1.removeEventListener('mouseenter', alertH1);
+};
+
+h1.addEventListener('mouseenter', alertH1);
+
+setTimeout(() => {
+  h1.removeEventListener('mouseenter', alertH1);
+}, 3000);
+
+//! 190강 - 이벤트 단계
+// 1. Capturing phase (캡쳐 단계) - 이벤트가 하위 요소로 전파되는 단계
+// a 태그에 click 이벤트를 추가하고 실제로 클릭하면
+// 이벤트가 a태그가 아닌 Document에서 Click event가 만들어진다. 이어서 캡쳐 단계가 아래에서 진행된다.
+// Document > html(Element) > body(Element) > section(Element) > p(Element) > a(Element)
+// Document에서 이벤트 발생지인 a 태그까지 내려온다. (이벤트 수신)
+
+// 2. Target Phase (타겟 단계) - 이벤트가 실제 타깃 요소에 전달되는 단계
+// Click event가 목적지인 a 태그에 도착하는 즉시, 목표 단계가 시작된다.
+// 이벤트가 대상에서 바로 처리될 수 있는 곳이다. .addEventListener()
+// 따라서 addEventListener()는 1단계인 캡쳐 단계가 종료 될 떄까지
+// Click event를 기다리고 있는 것이다. 이벤트가 도착하면 첨부된 콜백함수를 실행한다.
+
+// 3. Bubbling phase(버블링 단계) - 이벤트가 상위 요소로 전파되는 단계
+// 함수가 실행되면 다시 최하위 a태그 element부터 최상위 Document 까지 Click event를 전달한다.
+// (형제 요소가 아닌 부모 요소를 거쳐서 통과한다) 실제 이벤트의 처리는 3단계에서 진행된다.
+// 대부분의 이벤트는 캡쳐 & 버블링 과정을 거친다.
+
+//! 191강 - 이벤트 전파 예시
+const randomInt = (min, max) =>
+  Math.floor(Math.random() * (max - min + 1) + min);
+
+const randomColor = () =>
+  `rgb(${randomInt(0, 255)}, ${randomInt(0, 255)}, ${randomInt(0, 255)})`;
+
+console.log(randomInt(1, 3));
+// console.log(randomColor());
+// document.body.style.backgroundColor = randomColor();
+
+document.querySelector('.nav__link').addEventListener('click', function (e) {
+  this.style.backgroundColor = randomColor();
+  console.log('LINK', e.target, e.currentTarget);
+  console.log(e.currentTarget === this);
+});
+
+document.querySelector('.nav__links').addEventListener('click', function (e) {
+  this.style.backgroundColor = randomColor();
+  console.log('CONTAINER', e.target, e.currentTarget);
+});
+
+document.querySelector('.nav').addEventListener('click', function (e) {
+  this.style.backgroundColor = randomColor();
+  console.log('NAV', e.target, e.currentTarget);
 });
