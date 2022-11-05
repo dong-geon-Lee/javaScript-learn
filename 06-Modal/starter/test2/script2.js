b9dbcd706994ea02337fedc9b676cb0e89e59726
이동건 <developer@idong-geon-ui-MacBookPro.local>
2022-11-05 17:42:50 +0900

    Modal test completed

--- 06-Modal/starter/test2/script2.js
+++ 06-Modal/starter/test2/script2.js
@@ -1,1 +1,25 @@
 console.log('시작');
+
+const closeBtn = document.querySelector('#close--btn');
+const overlay = document.querySelector('.overlay');
+const modal = document.querySelector('.modal');
+const modalBtn = document.querySelectorAll('.modal--btn');
+
+console.log(modalBtn);
+
+modalBtn.forEach(btn =>
+  btn.addEventListener('click', () => {
+    overlay.classList.remove('hidden');
+    modal.classList.remove('hidden');
+  })
+);
+
+closeBtn.addEventListener('click', () => {
+  overlay.classList.add('hidden');
+  modal.classList.add('hidden');
+});
+
+overlay.addEventListener('click', () => {
+  overlay.classList.add('hidden');
+  modal.classList.add('hidden');
+});
