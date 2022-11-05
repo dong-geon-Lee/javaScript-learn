adc456dae25eece0c4a9452c2db0649c3b2d14f1
이동건 <developer@idong-geon-ui-MacBookPro.local>
2022-11-06 00:02:31 +0900

    dice app completed

--- 07-Pig-Game/starter/test2/script2.js
+++ 07-Pig-Game/starter/test2/script2.js
@@ -1,73 +1,107 @@
-const _doc = className => document.querySelector(className);
-const rollBtnEl = _doc('.roll-dice');
-const holdBtnEl = _doc('.hold');
-const resetBtnEl = _doc('.new-game');
-const player0El = _doc('.content__player-0');
-const player1El = _doc('.content__player-1');
-const diceEl = _doc('.dice--img');
-const score0El = _doc('.score__text--0');
-const score1El = _doc('.score__text--1');
-const curScore0El = _doc('.current__score--0');
-const curScore1El = _doc('.current__score--1');
-const playerBox = _doc('.content__box');
+const dice = document.querySelector('.dice');
+const rollDice = document.querySelector('#roll--dice');
+const cur1ScoreEl = document.querySelector('#current--score--1');
+const cur2ScoreEl = document.querySelector('#current--score--2');
+const player1Panel = document.querySelector('.player-panel--1');
+const player2Panel = document.querySelector('.player-panel--2');
+const holdBtn = document.querySelector('#hold');
+const displayScore1 = document.querySelector('#display--score--1');
+const displayScore2 = document.querySelector('#display--score--2');
+const newGame = document.querySelector('#new--game');
 
-let curScore = 0;
-let activePlayer = 0;
+let score = 0;
 let scores = [0, 0];
+let curScore = 0;
 let playing = true;
 
-const switchPlayer = () => {
-  if (scores[activePlayer] >= 20) {
-    endGame();
-  }
+rollDice.addEventListener('click', e => {
+  const target = e.target
+    .closest('.container')
+    .querySelector('.player--active');
 
-  _doc(`.current__score--${activePlayer}`).textContent = curScore = 0;
-  _doc(`.content__player-${activePlayer}`).classList.remove('player--active');
-  activePlayer = activePlayer === 0 ? 1 : 0;
-  _doc(`.current__score--${activePlayer}`).textContent = curScore = 0;
-  _doc(`.content__player-${activePlayer}`).classList.add('player--active');
-};
+  if (target && playing) {
+    const random = Math.floor(Math.random() * 6 + 1);
 
-const endGame = () => {
-  _doc(`.content__player-${activePlayer}`).classList.add('player--winner');
-  _doc(`.content__player-${activePlayer}`).classList.remove('player--active');
+    dice.classList.remove('hidden');
+    dice.src = `dice-${random}.png`;
+    const activePlayer1 = target.classList.contains('player-panel--1');
+    const activePlayer2 = target.classList.contains('player-panel--2');
 
-  playing = false;
-  rollBtnEl.disabled = true;
-  holdBtnEl.disabled = true;
-};
+    console.log(activePlayer1, activePlayer2);
 
-rollBtnEl.addEventListener('click', () => {
-  let diceNumber = Math.trunc(Math.random() * 6 + 1);
-  diceEl.src = `dice-${diceNumber}.png`;
-  diceEl.classList.remove('hidden');
+    if (activePlayer1) {
+      if (random === 1) {
+        cur1ScoreEl.textContent = curScore = 0;
+        player1Panel.classList.remove('player--active');
+        player2Panel.classList.add('player--active');
+        return;
+      }
 
-  if (diceNumber !== 1) {
-    curScore += diceNumber;
-    _doc(`.current__score--${activePlayer}`).textContent = curScore;
-  } else {
-    switchPlayer();
+      curScore += random;
+      cur1ScoreEl.textContent = curScore;
+    } else {
+      if (random === 1) {
+        cur2ScoreEl.textContent = curScore = 0;
+        player1Panel.classList.add('player--active');
+        player2Panel.classList.remove('player--active');
+        return;
+      }
+
+      curScore += random;
+      cur2ScoreEl.textContent = curScore;
+    }
   }
 });
 
-holdBtnEl.addEventListener('click', () => {
-  scores[activePlayer] += curScore;
-  _doc(`.score__text--${activePlayer}`).textContent = scores[activePlayer];
-  switchPlayer();
+holdBtn.addEventListener('click', e => {
+  const target = e.target
+    .closest('.container')
+    .querySelector('.player--active');
+
+  const activePlayer1 = target.classList.contains('player-panel--1');
+  const activePlayer2 = target.classList.contains('player-panel--2');
+
+  console.log(activePlayer1, activePlayer2);
+
+  if (playing) {
+    if (activePlayer1) {
+      scores[0] += +curScore;
+      displayScore1.textContent = scores[0];
+
+      if (scores[0] >= 20) {
+        player1Panel.classList.add('player--winner');
+        playing = false;
+        dice.classList.add('hidden');
+        return;
+      }
+      cur1ScoreEl.textContent = curScore = 0;
+      player1Panel.classList.remove('player--active');
+      player2Panel.classList.add('player--active');
+    } else {
+      scores[1] += +curScore;
+      displayScore2.textContent = scores[1];
+
+      if (scores[1] >= 20) {
+        player2Panel.classList.add('player--winner');
+        playing = false;
+        dice.classList.add('hidden');
+        return;
+      }
+      cur2ScoreEl.textContent = curScore = 0;
+      player1Panel.classList.add('player--active');
+      player2Panel.classList.remove('player--active');
+    }
+  }
 });
 
-resetBtnEl.addEventListener('click', () => {
-  curScore = scores[0] = scores[1] = activePlayer = 0;
-  score0El.textContent = 0;
-  score1El.textContent = 0;
-  curScore0El.textContent = 0;
-  curScore1El.textContent = 0;
+newGame.addEventListener('click', () => {
+  scores[0] = scores[1] = curScore = 0;
+  dice.classList.add('hidden');
+  displayScore1.textContent = displayScore2.textContent = 0;
+  cur1ScoreEl.textContent = cur2ScoreEl.textContent = 0;
+  player1Panel.classList.add('player--active');
+  player2Panel.classList.remove('player--active');
+  player1Panel.classList.remove('player--winner');
+  player2Panel.classList.remove('player--winner');
   playing = true;
-  rollBtnEl.disabled = false;
-  holdBtnEl.disabled = false;
-  diceEl.classList.add('hidden');
-  _doc('.content__player-0').classList.add('player--active');
-  _doc('.content__player-0').classList.remove('player--winner');
-  _doc('.content__player-1').classList.remove('player--active');
-  _doc('.content__player-1').classList.remove('player--winner');
 });
